011d5adefbf59a99bb8c4c0fd0740ac72753f2a4
takieeldeen <takie.eldeen1998@gmail.com>
2024-11-08 16:00:36 +0200

    Testing Error Case

--- app.ts
+++ app.ts
@@ -1,15 +1,16 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
 
-import { tourRouter } from './routes';
-import { userRouter } from './routes';
+import { tourRouter } from "./routes";
+import { userRouter } from "./routes";
 
 const app = express();
+const;
 // Third Party Middlewares
-if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
+if (process.env.NODE_ENV === "development") app.use(morgan("dev"));
 app.use(express.json());
 // Mounting Routers
-app.use('/api/v1/tours', tourRouter);
-app.use('/api/v1/users', userRouter);
+app.use("/api/v1/tours", tourRouter);
+app.use("/api/v1/users", userRouter);
 // Users Routes
 export default app;
