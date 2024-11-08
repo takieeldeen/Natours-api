d0dc275148c7742fef852a4b5d998471639c9495
takieeldeen <takie.eldeen1998@gmail.com>
2024-11-08 16:02:47 +0200

    Testing Error Case

--- dist/app.js
+++ dist/app.js
@@ -8,13 +8,14 @@ const morgan_1 = __importDefault(require("morgan"));
 const routes_1 = require("./routes");
 const routes_2 = require("./routes");
 const app = (0, express_1.default)();
+const ;
 // Third Party Middlewares
-if (process.env.NODE_ENV === 'development')
-    app.use((0, morgan_1.default)('dev'));
+if (process.env.NODE_ENV === "development")
+    app.use((0, morgan_1.default)("dev"));
 app.use(express_1.default.json());
 // Mounting Routers
-app.use('/api/v1/tours', routes_1.tourRouter);
-app.use('/api/v1/users', routes_2.userRouter);
+app.use("/api/v1/tours", routes_1.tourRouter);
+app.use("/api/v1/users", routes_2.userRouter);
 // Users Routes
 exports.default = app;
 //# sourceMappingURL=app.js.map
